8ca07a986e5b3a7e37c43803d02fdf0ee53a5d93
udeepika <pdeepika3112@gmail.com>
2013-07-06 21:30:11 -0700

    Revert "rearranged the project folder"
    This reverts commit abfc500873fab2e6dacf0a5f930b720d42b0702b.

--- MyMoneyMate/gen/com/example/moneymeterexample/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.example.moneymeterexample;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
---
+++ src/com/example/moneymeterexample/TableColumnConstants.java
@@ -0,0 +1,10 @@
+package com.example.moneymeterexample;
+
+public class TableColumnConstants {
+	
+	public static final String ID = "_id";
+	public static final String DATE = "date";
+	public static final String CATEGORY = "category";
+	public static final String AMOUNT = "amount";
+
+}
